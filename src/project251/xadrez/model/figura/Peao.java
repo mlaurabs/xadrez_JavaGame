ce096233d0e7062f8f65a5eb6afacbf816165e74
Guilherme Cunha <guicunhar@outlook.com>
2025-05-13 19:25:38 -0300

    Create jaMoveu para Peão, Torre e Rei

--- src/project251/xadrez/model/figura/Peao.java
+++ src/project251/xadrez/model/figura/Peao.java
@@ -3,10 +3,21 @@ package project251.xadrez.model.figura;
 import project251.xadrez.model.tabuleiro.Posicao;
 
 public class Peao extends Peca {
+	private boolean jaMoveu = false; 
 
 	public Peao(Posicao posicao) {
 		super(posicao);
 	}
+
+	public boolean getJaMoveu() { // se falso, pode andar duas casas na primeira rodada
+		return jaMoveu;
+	}
+
+
+	public void setJaMoveu(boolean jaMoveu) {
+		this.jaMoveu = jaMoveu;
+	}
+	
 
 	@Override
 	public String toString() {
--- src/project251/xadrez/model/figura/Torre.java
+++ src/project251/xadrez/model/figura/Torre.java
@@ -3,14 +3,22 @@ package project251.xadrez.model.figura;
 import project251.xadrez.model.tabuleiro.Posicao;
 
 public class Torre extends Peca {
+	private boolean jaMoveu = false; 
 
 	public Torre(Posicao posicao) {
 		super(posicao);
+	}
+
+	public boolean getJaMoveu() { //se falso, pode fazer o roque
+		return jaMoveu;
+	}
+
+	public void setJaMoveu(boolean jaMoveu) {
+		this.jaMoveu = jaMoveu;
 	}
 	
 	@Override
 	public String toString() {
 	    return "T";
 	}
-
 }
