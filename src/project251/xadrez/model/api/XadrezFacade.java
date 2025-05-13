95328592989093d745236255b33aebc3c7190d6f
Guilherme Cunha <guicunhar@outlook.com>
2025-05-12 21:28:58 -0300

    Update class Posicao

--- src/project251/xadrez/model/api/XadrezFacade.java
+++ src/project251/xadrez/model/api/XadrezFacade.java
@@ -1,5 +1,13 @@
 package project251.xadrez.model.api;
 
+import project251.xadrez.model.tabuleiro.Posicao;
+
 public class XadrezFacade {
 
+    public static void main(String[] args) {
+        Posicao p = new Posicao("e2");
+        System.out.println("Posição: " + p); // Deve imprimir "e2"
+        System.out.println("Linha: " + p.getLinha());  // Deve imprimir 6
+        System.out.println("Coluna: " + p.getColuna()); // Deve imprimir 4
+    }
 }
--- src/project251/xadrez/model/tabuleiro/Posicao.java
+++ src/project251/xadrez/model/tabuleiro/Posicao.java
@@ -1,5 +1,36 @@
 package project251.xadrez.model.tabuleiro;
 
-class Posicao {
+public class Posicao {
 
+    private int linha;
+    private int coluna;
+
+    public Posicao(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public Posicao(String notacao) {
+        if (notacao == null || notacao.length() != 2) { // se notacao for nulo ou de tamanho diferente de 2
+            throw new IllegalArgumentException("Notação inválida: " + notacao); // retorna um erro
+        }
+        char colunaChar = notacao.charAt(0); //coluna = primeiro caractere de notacao
+        char linhaChar = notacao.charAt(1); //linha = segundo caractere de notacao
+
+        this.coluna = colunaChar - 'a'; // transforma pra int
+        this.linha = 8 - Character.getNumericValue(linhaChar);
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    @Override
+    public String toString() {
+        return "" + (char) ('a' + coluna) + (8 - linha);
+    }
 }
